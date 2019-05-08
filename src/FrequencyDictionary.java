e3a16d4e87481178f84271d3a4bba8c4cf750b53
Spawn <alkspawn@gmail.com>
2019-05-08 20:27:27 +0300

    Finish him's

--- src/FrequencyDictionary.java
+++ src/FrequencyDictionary.java
@@ -1,10 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
-
-import static java.util.Arrays.sort;
+import java.util.regex.Pattern;
 
 public class FrequencyDictionary {
     private static final int LETTER_A = 97;
@@ -19,56 +16,65 @@ public class FrequencyDictionary {
     После оптимизации время выполнения стало 0.003-0.008 сек. Сам в шоке)))
 */
 
-    private File file = new File("D:\\Intelij project\\Lab8\\src\\FreqDictFile.txt");
+    private File file = new File("D:\\Intelij project\\Lab8\\src\\A tale two city.txt");
 
 
-    public void addCountArray(int[] array, char letter) {
+    private void addCountArray(long[] array, char letter) {
         int index = letter - LETTER_A;
-        array[index]++;
+        array[index]+=1;
     }
+
 
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        int[] array = new int[26];
+        long[] countArray = new long[26];
 
         FrequencyDictionary frequencyDictionary = new FrequencyDictionary();
         FileReader reader = new FileReader(frequencyDictionary.file);
         for (int i = 0; i < frequencyDictionary.file.length(); i++) {
             char letter = (char) Character.toLowerCase(reader.read());
-            if (Character.isLetter(letter)) {
-                frequencyDictionary.addCountArray(array, letter);
+            if (97<=letter&&letter>=122) {
+                frequencyDictionary.addCountArray(countArray, letter);
             }
         }
-        frequencyDictionary.printMessage(array);
+        frequencyDictionary.printMessage(frequencyDictionary.createArrayLetterNumber(countArray));
         long elapsedTimeMillis = System.currentTimeMillis() - start;
         float elapsedTimeSec = elapsedTimeMillis / 1000F;
         System.out.println("Program working time: " + elapsedTimeSec + " sec.");
     }
 
-    public int getIndex(int number, int[] array, int count) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == number) {
-                return i + count;
-            }
+    private Object[][] createArrayLetterNumber(long[] countArray) {
+        Object[][] finalArray = new Object[countArray.length][2];
+        for (int i = 0; i < finalArray.length; i++) {
+            finalArray[i][0] = Character.valueOf((char) (LETTER_A + i));
+            finalArray[i][1] = countArray[i];
         }
-        return 0;
+        return finalArray;
     }
 
-    private void printMessage(int[] array) {
-        int[] sortArray = Arrays.copyOf(array, array.length);
-        Arrays.sort(sortArray);
-        int count = 0;
-        for (int j = sortArray.length - 1; j > 0; j--) {
-            if (sortArray[j] != 0) {
-                int index = getIndex(sortArray[j], array, count);
-                count++;
-                char letter = Character.valueOf((char) (LETTER_A + index));
-                System.out.println(letter + " - " + sortArray[j]);
-            } else {
-                count=0;
+    private void arraysSort(Object[][] array) {
+        Arrays.sort(array, new someComparator());
+    }
+
+    private class someComparator implements Comparator<Object[]> {
+        public int compare(Object[] row1, Object[] row2) {
+            long value1 = (long) row1[1];
+            long value2 = (long) row2[1];
+            char key1 = (char) row1[0];
+            char key2 = (char) row2[0];
+            if (value1 == value2 && key1 < key2) {
+                return Character.compare(key1, key2);
+            }
+            return Long.compare(value2, value1);
+        }
+    }
+
+    private void printMessage(Object[][] finalArray) {
+        arraysSort(finalArray);
+        for (int i = 0; i < finalArray.length; i++) {
+            if ((long) finalArray[i][1] != 0) {
+                System.out.println(finalArray[i][0] + " - " + finalArray[i][1]);
             }
         }
-               /* System.out.println(letter + " - " + array[j]);
-                System.out.println(sortArray[j]);*/
     }
 }
